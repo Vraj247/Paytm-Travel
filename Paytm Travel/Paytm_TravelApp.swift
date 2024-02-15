e59fd0fab5b0e5e24c1e610f4161960a22bfda62
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-02-15 11:54:28 +0530

    Initial Commit

---
+++ Paytm Travel/Paytm_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paytm_TravelApp.swift
+//  Paytm Travel
+//
+//  Created by Vikas Yadav on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Paytm_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
